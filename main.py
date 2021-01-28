c4fb337f4d2f214f134e173ca0cf07c5bf45d3e6
BatesyBoy <19lbates@kes.net>
2021-01-28 11:46:33 +0000

    Option to not clear the canvas

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ while True:
 	sides=int(input("> INPUT POLYGON SIDES (3-12) "))
 	thick=int(input("> INPUT PEN THICKNESS (1-10) "))
 	colour=input("> INPUT COLOUR ").lower()
-	sideloop=sides
 	if sides>12 or sides<3 or thick>10 or thick<1:
 		print("> INVALID INPUT- TRY AGAIN")
 	else:
 		t.color(colour)
-		while sideloop>0:
-			t.pensize(thick)
+		t.pensize(thick)
+		for x in range(0,sides):
 			t.forward(500/sides)
 			t.right(360/sides)
-			sideloop=sideloop-1
 		rerun=input("> RERUN PROGRAM (Y/N) ").upper()
 		if not rerun=="Y":
 			print("> PROGRAM ENDED")
 			break
 		else:
-			t.reset()
+			clear=input("> CLEAR CANVAS (Y/N)").upper()
+			if clear=="Y":
+				t.reset()
 			print("> RERUNNING PROGRAM")
