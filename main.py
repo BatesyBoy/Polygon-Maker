8f147beea45850958cbf07cb70eecbd9b061cf78
BatesyBoy <19lbates@kes.net>
2021-01-28 11:34:11 +0000

    Thickness And Colour Flexibility

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import turtle
 print("============================\n========POLYGON MAKER=======\n============================")
 t = turtle.Turtle()
 while True:
-	sides=int(input("> INPUT POLYGON SIDES (3-100) "))
+	sides=int(input("> INPUT POLYGON SIDES (3-12) "))
+	thick=int(input("> INPUT PEN THICKNESS (1-10) "))
+	colour=input("> INPUT COLOUR (RED,BLUE,BLACK,GREEN,YELLOW) ").upper()
 	sideloop=sides
-	if sides>100 or sides<3:
+	if sides>12 or sides<3 or thick>10 or thick<1:
 		print("> INVALID INPUT- TRY AGAIN")
 	else:
+		if colour=="RED":
+			t.color("red")
+		elif colour=="BLUE":
+			t.color("blue")
+		elif colour=="BLACK":
+			t.color("black")
+		elif colour=="GREEN":
+			t.color("green")
+		elif colour=="YELLOW":
+			t.color("yellow")
 		while sideloop>0:
+			t.pensize(thick)
 			t.forward(500/sides)
 			t.right(360/sides)
 			sideloop=sideloop-1
