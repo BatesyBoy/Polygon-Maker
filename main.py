1af0eff4ba4428d257d85cc877dbc002c418c2ed
BatesyBoy <19lbates@kes.net>
2021-01-28 11:39:25 +0000

    Less Lines

--- main.py
+++ main.py
@@ -4,21 +4,12 @@ t = turtle.Turtle()
 while True:
 	sides=int(input("> INPUT POLYGON SIDES (3-12) "))
 	thick=int(input("> INPUT PEN THICKNESS (1-10) "))
-	colour=input("> INPUT COLOUR (RED,BLUE,BLACK,GREEN,YELLOW) ").upper()
+	colour=input("> INPUT COLOUR ").lower()
 	sideloop=sides
 	if sides>12 or sides<3 or thick>10 or thick<1:
 		print("> INVALID INPUT- TRY AGAIN")
 	else:
-		if colour=="RED":
-			t.color("red")
-		elif colour=="BLUE":
-			t.color("blue")
-		elif colour=="BLACK":
-			t.color("black")
-		elif colour=="GREEN":
-			t.color("green")
-		elif colour=="YELLOW":
-			t.color("yellow")
+		t.color(colour)
 		while sideloop>0:
 			t.pensize(thick)
 			t.forward(500/sides)
